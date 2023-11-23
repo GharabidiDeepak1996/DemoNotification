0b502715b239bf42c629eed629a02af5c8f3ef43
dgharabidi <dgharabidi@gmail.com>
2023-11-23 23:32:54 +0530

    Initial commit

---
+++ app/src/main/java/com/example/demonotification/MyFirebaseMessagingService.java
@@ -0,0 +1,26 @@
+package com.example.demonotification;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onNewToken(String token) {
+        super.onNewToken(token);
+        Log.d("token", "onNewToken: "+token);
+       // Log.e("newToken", token);
+//Add your token in your sharepreferences.
+        getSharedPreferences("_", MODE_PRIVATE).edit().putString("fcm_token", token).apply();
+    }
+
+//    @Override
+//    public void onMessageReceived(RemoteMessage remoteMessage) {
+//        super.onMessageReceived(remoteMessage);
+//    }
+//    //Whenewer you need FCM token, just call this static method to get it.
+//    public static String getToken(Context context) {
+//        return context.getSharedPreferences("_", MODE_PRIVATE).getString("fcm_token", "empty");
+//    }
+}
